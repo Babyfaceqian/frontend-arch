c87a31b0cc2afe7b05a5c3870d16614d27db1e4c
babyfaceqian <babyfaceqian92@gmail.com>
2019-04-18 17:22:27 +0800

    feat: 增加图标

---
+++ data/data.js
@@ -0,0 +1,37 @@
+var data = [
+  {
+    time: '2',
+    persons: [
+      {
+        name: '周润发',
+        duration: '200'
+      }
+    ]
+  },
+  {
+    time: '2',
+    persons: [
+      {
+        name: '周润发',
+        duration: '200'
+      },
+      {
+        name: '张国荣',
+        duration: '200'
+      }
+    ]
+  },
+  {
+    time: '3',
+    persons: [
+      {
+        name: '周润发',
+        duration: '200'
+      },
+      {
+        name: '张国荣',
+        duration: '200'
+      }
+    ]
+  },
+]
