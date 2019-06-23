abccb39d468e11c67e67f1ae99af568506ac4aa7
babyfaceqian <babyfaceqian92@gmail.com>
2019-06-23 12:34:46 +0800

    update

---
+++ index.js
@@ -0,0 +1,30 @@
+! function () {
+  /** 
+   * d3.now()
+   */
+  console.log('d3.now():', d3.now());
+
+  /**
+   * d3.timer(callback[, delay[, time]]) 
+   */
+
+  /**
+   * timer.restart(callback[, delay[, time]])
+   */
+
+  /**
+   * timer.stop()
+   */
+
+  /**
+   * d3.timerFlush()
+   */
+
+  /**
+   * d3.timeout(callback[, delay[, time]])
+   */
+
+  /**
+   * d3.interval(callback[, delay[, time]]) 
+   */
+}()
