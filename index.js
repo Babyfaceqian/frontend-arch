ae6fb7f8974d00f9945b32f6b352cbd349035833
babyfaceqian <babyfaceqian92@gmail.com>
2019-06-23 18:06:49 +0800

    udpate

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 ! function () {
 
   /**
+   * d3.transition([name])
+   * 返回一个新的transition。
    * selection.transition([name])
+   * 返回以name为名字的transition，如果没有，则返回新的。
    */
-  var t = d3.transition()
+  var t = d3.transition('first')
     .duration(2000)
     .ease(d3.easeLinear);
 
@@ -11,43 +14,50 @@
   var d = d3.select('body').selectAll('.block').data(data);
   var block = d.enter().append('div').attr('class', 'block');
   block.style('width', '100px').style('height', '100px');
-  block.style('background', 'white');
-  block.transition(t).style('background', 'blue');
+  block.style('background', 'grey');
+  block.transition('second').duration(2000).style('background', 'blue');
+  block.transition(t).style('width', '200px');
   /**
    * selection.interrupt([name])
    * d3.interrupt(node[, name]) // 只能对单个node
    */
-  setTimeout(function () {
-    block.interrupt()
-    // d3.interrupt(block.node())
-  }, 500);
-
-  /**
-   * d3.transition([name])
-   */
+  block.interrupt('first');
+  d3.interrupt(block.node(), 'second')
 
   /**
    * transition.select(selector)
+   * selector可以为选择器也可以function，如果为function则参数为d, i, this，需要返回selection；返回匹配的第一个selection。
+   * transition.selectAll(selector)
+   * 返回匹配的所有selection。
    */
+  block.transition('third').duration(2000).select(function (d, i) {
+    if (i !== 0) {
+      return this;
+    }
+  }).style('height', '200px')
 
   /**
    * transition.filter(filter)
+   * filter可以为选择器也可以function，如果为function则参数为d, i, this，需要返回selection；
    */
-
-  var t2 = d3.transition().duration(2000)
-    .ease(d3.easeLinear);
-  block.transition(t2).filter(function (d, i) {
-    console.log('filter', d, i)
-    return i !== 0;
-  }).style('background', 'red');
+  block.transition('fourth').duration(2000).filter(function (d, i) {
+    return i == 0;
+  }).style('margin-left', '50px')
 
   /**
    * transition.merge(other) 
    */
-  var block2 = d3.select('body').append('div').attr('class', 'block2');
-  var t3 = d3.transition().duration(2000)
-  block2.transition(t3);
+  var fifth = block.transition('fifth').duration(2000);
+  block.transition(fifth).merge(fifth.delay(2000)).style('margin-top', '50px');
+  // block.transition(fifth).delay(2000).style('margin-top', '50px'); 同上
+
+  /**
+   * transition.transition()
+   */
+  var sixth = d3.transition().duration(2000).delay(2000);
+  block.transition(sixth).style('margin-left', '100px').transition(sixth).style('margin-left', '150px')
   /**
    * d3.active(node[, name])
+   * 返回激活的transition
    */
 }()
