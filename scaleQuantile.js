a5ccb1489187ffd64cdf08306aec4bcb39eeecf6
babyfaceqian <babyfaceqian92@gmail.com>
2019-08-02 14:58:57 +0800

    fix

--- scaleQuantile.js
+++ scaleQuantile.js
@@ -4,7 +4,7 @@
    */
   // 分位比例尺，分位值与定义域中存在的数值都有关
   let quantile = d3.scaleQuantile()
-    .domain([0, 2, 4, 10]) // 定义域
+    .domain([2, 4]) // 定义域
     .range([0, 100, 200]); // 值域
-    console.log(quantile(3)); // 100, 获得离散数据
+    console.log(quantile(9)); // 100, 获得离散数据
 })()
