30fb394537cf7a4a3fca3e725f3633c5dd285750
babyfaceqian <babyfaceqian92@gmail.com>
2019-08-16 15:48:26 +0800

    fix: 更新babel,更新webpack配置,支持异步

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ This branch is a react arch base which includes the following open source librar
 babel, webpack, eslint, react
 
 Any branch checkouted from this branch will focus on react.
+
+- 支持async/await
+- 
--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import styles from './Main.less';
-
+import worm from 'utils/worm';
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {}
   }
   componentWillMount() {
   }
-  componentDidMount() {
+  async componentDidMount() {
+    let res = await worm.get('https://www.apiopen.top/weatherApi', { city: '杭州' })
+    if (res && res.code == 200) {
+      this.setState({ data: res.data })
+    }
   }
   render() {
     return (
-      <div className={styles.main}>This is React Arch branch.</div>
+      <div className={styles.main}></div>
     );
   }
 }
